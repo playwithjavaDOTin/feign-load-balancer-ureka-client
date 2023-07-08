dee09bf9c1e811012029a3ddaa4e50f9446f8d40
aray <aray@altimetrik.com>
2023-07-08 15:59:20 +0530

    initial commit by Admin

---
+++ src/main/java/playwithjava/in/service/EmpRemoteService.java
@@ -0,0 +1,9 @@
+package playwithjava.in.service;
+
+import playwithjava.in.bean.EmployeeResponse;
+
+
+public interface EmpRemoteService {
+public EmployeeResponse getRemoteEmployeeById(Long empId);
+
+}
